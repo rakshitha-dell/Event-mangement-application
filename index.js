913c0c201f557c501914371f135d45951cabade3
rakshitha <devadigarakshitha874@gmail.com>
2025-10-04 11:16:23 +0530

    added

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
-const port = 3010;
+const PORT = 3000;
 
 app.use(express.static('static'));
+
+mongoose.connect(process.env.MONGO_URI, {UseNewUserParser: true, UseUnifiedTopology: true})
+.then(() =>console.log("Connected to MongoDB"))
+.catch(()=> console.log("Error connecting to MongoDB"));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
